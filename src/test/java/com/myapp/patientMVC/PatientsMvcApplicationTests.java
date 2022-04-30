4a784347d2769b431e2d9067012e72a4803fcb31
IssamBen98 <benatik.issam@gmail.com>
2022-04-30 00:19:24 +0000

    Partie1

---
+++ src/test/java/com/myapp/patientMVC/PatientsMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myapp.patientMVC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientsMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
